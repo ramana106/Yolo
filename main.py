c5600cd2340631860c1cedc02ace064b0df87a6c
Ramanathan L <ramanathan@madstreetden.com>
2019-05-18 14:25:41 +0530

    added screen capture

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import os
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+
+mon = {'top': 175, 'left': 20, 'width': 1010, 'height': 325}
+
+sct = mss()
+co = 0
+while (co < 50):
+    print co
+    sct.get_pixels(mon)
+    img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+    cv2.imwrite('./out/{}.jpg'.format(str(co)), np.array(img, dtype='uint8'))
+    co += 1
