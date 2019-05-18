1df99f1bebd17ebb2a8ff17249174f85f142be99
Ramanathan L <ramanathan@madstreetden.com>
2019-05-18 14:31:57 +0530

    added req

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ mon = {'top': 175, 'left': 20, 'width': 1010, 'height': 325}
 sct = mss()
 co = 0
 while (co < 50):
-    print co
+    print (co)
     sct.get_pixels(mon)
     img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
     cv2.imwrite('./out/{}.jpg'.format(str(co)), np.array(img, dtype='uint8'))
